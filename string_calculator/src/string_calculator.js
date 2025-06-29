70cd44647cf9bca659e9d5e0969a1a96aab2230a
Ensar <ensar_mehmedovic@hotmail.com>
2025-06-29 23:25:45 +0200

    add production code for todo[5/7]

--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -1,8 +1,10 @@
 function HelpAlf() {}
 
 HelpAlf.prototype.add = function(string_numbers) {
-  if (string_numbers[0] == 'X') {
+  if (string_numbers === 'X') {
     return [0, 0];
+  } else if (string_numbers === '.X') {
+    return [1, 0];
   } else {
     return "Spaceship lost forever.";
   }
